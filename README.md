c55f39cca6e18348ac2c7546f048085dbe388234
Denzil Brade <denzbrade@gmail.com>
2017-10-23 11:45:37 +0100

    Update ReadMe & Comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ostModernTest
+# React JS Set Project
+
+To get started please clone or download this project to your local machine.
+
+- Install NPM: `npm install`
+- To start the local server: `npm start`
+- Navigate to local port 8080 (e.g. **localhost/8080**)
+
+(Reminder Enable Allow Control Origin - Chrome extension if data is not loading *check console for errors* )
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,8 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
-        <label htmlFor="search">You searched for:{this.state.term}</label>
+        <label htmlFor="search">The set you searched for:{this.state.term}</label>
+        {/* Search Bar that returns your search term as you type */}
         <input className="searchBar form-control" id="search" placeholder="Search here... (Fake search)"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)}
